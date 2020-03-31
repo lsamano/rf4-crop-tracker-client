c20e245a5a9a813ba12644dea8845300aa384f6b
lsamano <leizl.samano@gmail.com>
2020-03-31 10:47:28 -0400

    refactored table to its own component

---
+++ src/GoodCropsTable.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import CropRow from './CropRow';
+// import PropTypes from 'prop-types'
+
+const GoodCropsTable = ({ goodCrops }) => {
+  const [ sortBy, setSortBy ] = useState("")
+
+  const handleClick = event => {
+    const clickedText = event.target.innerText
+    if (sortBy === clickedText) {
+      setSortBy("")
+    } else {
+      setSortBy(clickedText)
+    }
+  }
+
+  const activeCheck = category => {
+    if (sortBy === category) {
+      return "active"
+    } else {
+      return ""
+    }
+  }
+
+  const makeHeaderRows = () => {
+    const headers = ["Name", "Growth Time", "Harvest Amount", "Regrows?"]
+    return headers.map(header => {
+      return (
+        <th
+          onClick={handleClick}
+          className={activeCheck(header)}>
+          {header}
+        </th> )
+    })
+  }
+
+  const formatGoodCrops = () => {
+    let sortedCrops = [...goodCrops]
+    if (sortBy === "Name") {
+      sortedCrops = sortedCrops.sort((a, b) => a.name.localeCompare(b.name))
+    } else if (sortBy === "Growth Time") {
+      sortedCrops = sortedCrops.sort(((a, b) => a.grow_time - b.grow_time))
+    } else if (sortBy === "Harvest Amount") {
+      sortedCrops = sortedCrops.sort(((a, b) => a.harvest_amount - b.harvest_amount))
+    } else if (sortBy === "Regrows?") {
+      sortedCrops = sortedCrops.sort(((a, b) => b.regrows - a.regrows))
+    }
+    return sortedCrops.map(crop => {
+      return <CropRow key={crop.id} {...crop} />
+    })
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          { makeHeaderRows() }
+          {/*<th>Level</th>*/}
+        </tr>
+      </thead>
+      <tbody>{ formatGoodCrops() }</tbody>
+    </table>
+  )
+}
+
+export default GoodCropsTable
--- src/Season.jsx
+++ src/Season.jsx
@@ -1,81 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import CropRow from './CropRow';
+import GoodCropsTable from './GoodCropsTable';
 
 const Season = ({ name, liked_crops, liked_flowers }) => {
-  const [ sortBy, setSortBy ] = useState("")
-
-  const handleClick = event => {
-    const clickedText = event.target.innerText
-    if (sortBy === clickedText) {
-      setSortBy("")
-    } else {
-      setSortBy(clickedText)
-    }
-  }
-
-  const formatGoodCrops = () => {
-    let sortedCrops = [...liked_crops]
-    if (sortBy === "Name") {
-      sortedCrops = sortedCrops.sort((a, b) => a.name.localeCompare(b.name))
-    } else if (sortBy === "Growth Time") {
-      sortedCrops = sortedCrops.sort(((a, b) => a.grow_time - b.grow_time))
-    } else if (sortBy === "Harvest Amount") {
-      sortedCrops = sortedCrops.sort(((a, b) => a.harvest_amount - b.harvest_amount))
-    } else if (sortBy === "Regrows?") {
-      sortedCrops = sortedCrops.sort(((a, b) => b.regrows - a.regrows))
-    }
-    return sortedCrops.map(crop => {
-      return <CropRow key={crop.id} {...crop} />
-    })
-  }
-
-  const formatGoodFlowers = () => {
-    return liked_flowers.map(crop => {
-      return <CropRow key={crop.id} {...crop} />
-    })
-  }
-
-  const activeCheck = category => {
-    if (sortBy === category) {
-      return "active"
-    } else {
-      return ""
-    }
-  }
-
-  const makeHeaderRows = () => {
-    const headers = ["Name", "Growth Time", "Harvest Amount", "Regrows?"]
-    return headers.map(header => {
-      return (
-        <th
-          onClick={handleClick}
-          className={activeCheck(header)}>
-          {header}
-        </th> )
-    })
-  }
+  // const formatGoodFlowers = () => {
+  //   return liked_flowers.map(crop => {
+  //     return <CropRow key={crop.id} {...crop} />
+  //   })
+  // }
 
   return (
     <div className="seasons">
     <h1>{name.toUpperCase()}</h1>
+    <GoodCropsTable goodCrops={liked_crops} />
     <table>
       <thead>
         <tr>
-          { makeHeaderRows() }
+          { null }
           {/*<th>Level</th>*/}
         </tr>
       </thead>
-      <tbody>{formatGoodCrops()}</tbody>
-    </table>
-    <table>
-      <thead>
-        <tr>
-          { makeHeaderRows() }
-          {/*<th>Level</th>*/}
-        </tr>
-      </thead>
-      <tbody>{formatGoodFlowers()}</tbody>
+      <tbody>{null}</tbody>
     </table>
   </div>
   );
