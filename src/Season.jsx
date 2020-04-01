2300a87a0645437da539d18b8c774b0149a61434
lsamano <leizl.samano@gmail.com>
2020-04-01 17:56:06 -0400

    cleanup

--- src/Season.jsx
+++ src/Season.jsx
@@ -3,12 +3,6 @@ import PropTypes from 'prop-types';
 import GoodCropsTable from './GoodCropsTable';
 
 const Season = ({ name, liked_crops, liked_flowers }) => {
-  // const formatGoodFlowers = () => {
-  //   return liked_flowers.map(crop => {
-  //     return <CropRow key={crop.id} {...crop} />
-  //   })
-  // }
-
   return (
     <div className="seasons">
     <h1>{name.toUpperCase()}</h1>
