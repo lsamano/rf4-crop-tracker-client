e80398a749fcc5e44b6a8c15cc61c8f8ff342e17
lsamano <leizl.samano@gmail.com>
2020-06-07 06:19:34 -0400

    neutral table added

--- src/Season.jsx
+++ src/Season.jsx
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 import GoodCropsTable from './GoodCropsTable';
 import GoodFlowersTable from './GoodFlowersTable';
 
-const Season = ({ name, liked_crops, liked_flowers }) => {
+const Season = ({ name, liked_crops, liked_flowers, neutral_crops }) => {
   // <h2>{name.toUpperCase()}</h2>
   return (
     <>
       <div className="seasons">
         <GoodCropsTable goodCrops={liked_crops} />
         <GoodFlowersTable goodFlowers={liked_flowers} />
+        <GoodCropsTable goodCrops={neutral_crops} />
       </div>
     </>
   );
