e6369db5242b3068d287145f1d3582fb67dfd42a
lsamano <leizl.samano@gmail.com>
2020-06-12 22:40:08 -0400

    added toggle for seeing hated crops

--- src/Season.jsx
+++ src/Season.jsx
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import CropsTable from './CropsTable';
 
-const Season = ({ name, liked_crops, liked_flowers, neutral_crops }) => {
+const Season = ({
+  name,
+  liked_crops, liked_flowers,
+  neutral_crops,
+  hated_crops, hated_flowers
+}) => {
+  const [ likeToggle, setLikeToggle ] = useState(false)
+
+  const showSeason = () => {
+    if (likeToggle) {
+      return (
+        <div className="seasons">
+          <CropsTable crops={liked_crops} regrow={true} title={"Crops"} />
+          {liked_flowers.length > 0
+            && <CropsTable crops={liked_flowers} regrow={false} title={"Flowers"} />}
+            {neutral_crops.length > 0
+              && <CropsTable crops={neutral_crops} regrow={true} title={"Neutrals"} />}
+            </div>
+          );
+        } else {
+          return (
+            <div className="seasons">
+              <CropsTable crops={hated_crops} regrow={true} title={"Hated Crops"} />
+              { hated_flowers.length > 0
+                && <CropsTable crops={hated_flowers} regrow={false} title={"Hated Flowers"} />}
+              </div>
+            )
+          }
+  }
+
   return (
-      <div className="seasons">
-        <CropsTable crops={liked_crops} regrow={true} title={"Crops"} />
-        {liked_flowers.length > 0
-          && <CropsTable crops={liked_flowers} regrow={false} title={"Flowers"} />}
-        {neutral_crops.length > 0
-          && <CropsTable crops={neutral_crops} regrow={true} title={"Neutrals"} />}
-      </div>
-  );
+    <div>
+      <button onClick={() => setLikeToggle(!likeToggle)}>
+        {likeToggle ? "Click to Show Out-of-Season" : "Click to Show In-Season and Neutrals"}
+      </button>
+      { showSeason() }
+    </div>
+  )
 }
 
 Season.propTypes = {
